317eb17bd412b594b06910e90b2663254cc12c49
vagrant <vagrant@homestead>
2019-02-04 10:23:08 +0000

    Added real life examples to track and lesson seeders

--- database/seeds/LessonSeeder.php
+++ database/seeds/LessonSeeder.php
@@ -15,18 +15,18 @@ class LessonSeeder extends Seeder
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lektion 3.1',
+            'name' => '1.1 - Anställningen',
             'locale' => 'sv',
-            'description' => 'Beskrivning av lektion 3.1'
+            'description' => 'Om verksamhetens rutiner och förväntningar. Din roll som medarbetare är viktig!'
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lesson 3.1',
+            'name' => '1.1 - The employment',
             'locale' => 'en',
-            'description' => 'Description of lesson 3.1'
+            'description' => 'About the business\'s routines and expectations. Your role as an employee is important!'
         ]);
         DB::table('lesson_track')->insert([
-            'track_id' => 3,
+            'track_id' => 1,
             'lesson_id' => $id
         ]);
 
@@ -34,16 +34,16 @@ class LessonSeeder extends Seeder
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lektion 3.2',
+            'name' => '1.2 - Bemötande',
             'locale' => 'sv'
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lesson 3.2',
+            'name' => '1.2 - Treatment',
             'locale' => 'en'
         ]);
         DB::table('lesson_track')->insert([
-            'track_id' => 3,
+            'track_id' => 1,
             'lesson_id' => $id
         ]);
 
@@ -51,16 +51,84 @@ class LessonSeeder extends Seeder
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lektion 3.3',
+            'name' => '1.3 - Dokumentation och sekretess',
             'locale' => 'sv'
         ]);
         DB::table('lesson_translations')->insert([
             'lesson_id' => $id,
-            'name' => 'Lesson 3.3',
+            'name' => '1.3 - Documentation and secrecy',
             'locale' => 'en'
         ]);
         DB::table('lesson_track')->insert([
-            'track_id' => 3,
+            'track_id' => 1,
+            'lesson_id' => $id
+        ]);
+
+        $id = DB::table('lessons')->insertGetId([
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.4 - Omvårdnadsarbete',
+            'locale' => 'sv'
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.4 - Nursing',
+            'locale' => 'en'
+        ]);
+        DB::table('lesson_track')->insert([
+            'track_id' => 1,
+            'lesson_id' => $id
+        ]);
+
+        $id = DB::table('lessons')->insertGetId([
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.5 - Måltider och andra aktiviteter',
+            'locale' => 'sv'
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.5 - Meals and other activities',
+            'locale' => 'en'
+        ]);
+        DB::table('lesson_track')->insert([
+            'track_id' => 1,
+            'lesson_id' => $id
+        ]);
+
+        $id = DB::table('lessons')->insertGetId([
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.6 - Ergonomi och förflyttningsteknik',
+            'locale' => 'sv'
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.6 - Ergonomics and transfer techniques',
+            'locale' => 'en'
+        ]);
+        DB::table('lesson_track')->insert([
+            'track_id' => 1,
+            'lesson_id' => $id
+        ]);
+
+        $id = DB::table('lessons')->insertGetId([
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.7 - Palliativ vård',
+            'locale' => 'sv'
+        ]);
+        DB::table('lesson_translations')->insert([
+            'lesson_id' => $id,
+            'name' => '1.7 - Palliative care',
+            'locale' => 'en'
+        ]);
+        DB::table('lesson_track')->insert([
+            'track_id' => 1,
             'lesson_id' => $id
         ]);
 
--- database/seeds/TrackSeeder.php
+++ database/seeds/TrackSeeder.php
@@ -16,12 +16,12 @@ class TrackSeeder extends Seeder
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 1,
-            'name' => 'Spår 1',
+            'name' => 'Spår 1 - Introduktion äldreomsorg',
             'locale' => 'sv'
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 1,
-            'name' => 'Track 1',
+            'name' => 'Track 1 - Introduction elder care',
             'locale' => 'en'
         ]);
 
@@ -30,12 +30,12 @@ class TrackSeeder extends Seeder
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 2,
-            'name' => 'Spår 2',
+            'name' => 'Spår 2 - Vårdens grunder och teori',
             'locale' => 'sv'
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 2,
-            'name' => 'Track 2',
+            'name' => 'Track 2 - The bases and theory of care',
             'locale' => 'en'
         ]);
 
@@ -44,12 +44,12 @@ class TrackSeeder extends Seeder
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 3,
-            'name' => 'Spår 3',
+            'name' => 'Spår 3 - Vara människa',
             'locale' => 'sv'
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 3,
-            'name' => 'Track 3',
+            'name' => 'Track 3 - Being human',
             'locale' => 'en'
         ]);
 
@@ -58,12 +58,68 @@ class TrackSeeder extends Seeder
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 4,
-            'name' => 'Spår 4',
+            'name' => 'Spår 4 - Diskriminering',
             'locale' => 'sv'
         ]);
         DB::table('track_translations')->insert([
             'track_id' => 4,
-            'name' => 'Track 4',
+            'name' => 'Track 4 - Discrimination',
+            'locale' => 'en'
+        ]);
+
+        DB::table('tracks')->insert([
+            'id' => 5,
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 5,
+            'name' => 'Spår 5 - Äldres hälsa och livskvalitet',
+            'locale' => 'sv'
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 5,
+            'name' => 'Track 5 - Health and quality of life for elderly',
+            'locale' => 'en'
+        ]);
+
+        DB::table('tracks')->insert([
+            'id' => 6,
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 6,
+            'name' => 'Spår 6 - Funktionsnedsättning (LSS)',
+            'locale' => 'sv'
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 6,
+            'name' => 'Track 6 - Disabilities (LSS)',
+            'locale' => 'en'
+        ]);
+
+        DB::table('tracks')->insert([
+            'id' => 7,
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 7,
+            'name' => 'Spår 7 - Demens',
+            'locale' => 'sv'
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 7,
+            'name' => 'Track 7 - Dementia',
+            'locale' => 'en'
+        ]);
+
+        DB::table('tracks')->insert([
+            'id' => 8,
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 8,
+            'name' => 'Spår 8',
+            'locale' => 'sv'
+        ]);
+        DB::table('track_translations')->insert([
+            'track_id' => 8,
+            'name' => 'Track 8',
             'locale' => 'en'
         ]);
     }

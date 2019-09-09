7eb8d0eebfc1d6037ce3ec220d920c80b7312934
DanielMalmgren <daniel@kolefors.se>
2019-09-09 10:07:28 +0000

    Updated translations

--- resources/views/pages/unsecurelogin.blade.php
+++ resources/views/pages/unsecurelogin.blade.php
@@ -49,7 +49,7 @@
 
                                 {{--@if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('Glömt ditt lösenord?') }}
                                     </a>
                                 @endif--}}
                             </div>

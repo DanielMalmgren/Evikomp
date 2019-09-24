28c7cb980509727a6d76da906d0a911a701546a6
DanielMalmgren <daniel@kolefors.se>
2019-09-24 07:46:37 +0000

    Fix separation of names for users with double first names

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -16,7 +16,7 @@ function add_flash_message(array $notification){
 
 function str_word_count_utf8(string $str, int $format = 0) {
     if($format === 1) {
-        return preg_split('~[^\p{L}\p{N}\']+~u',$str);
+        return preg_split('~[^\p{L}\p{N}\'-]+~u',$str);
     }
-    return count(preg_split('~[^\p{L}\p{N}\']+~u',$str));
+    return count(preg_split('~[^\p{L}\p{N}\'-]+~u',$str));
 }

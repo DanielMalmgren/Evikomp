540050df796e9893b649fc14696889abca3ed6f1
DanielMalmgren <daniel@kolefors.se>
2019-09-24 12:48:35 +0000

    Hopefully fixed another name problem

--- app/Listeners/LoginListener.php
+++ app/Listeners/LoginListener.php
@@ -53,13 +53,15 @@ class LoginListener
                 logger("SAML Mailadress: ".$userattr["urn:oid:0.9.2342.19200300.100.1.3"][0]);
             }
             $user->personid = $personnr;
-            if(str_word_count($firstname) === 1) {
+            if(str_word_count_utf8($firstname) === 1) {
                 $user->firstname = $firstname;
             } else {
-                $user->firstname = str_word_count($firstname, 1)[0];
+                $user->firstname = str_word_count_utf8($firstname, 1)[0];
             }
+
+            $user->name = $firstname." ".$lastname;
         }
-        $user->name = $firstname." ".$lastname;
+
         $user->saml_firstname = $firstname;
         $user->lastname = $lastname;
         $user->save();

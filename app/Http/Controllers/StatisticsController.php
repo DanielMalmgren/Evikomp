05f2d2b53f986fe7716180a11b01dae12cafa37a
DanielMalmgren <daniel@kolefors.se>
2019-11-06 10:14:13 +0000

    More statistics!

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\ActiveTime;
+use App\ProjectTime;
+use App\TimeAttest;
 use App\User;
 use App\Workplace;
 use App\Lesson;
 use App\Charts\SessionChart;
-use Carbon\Carbon;
 
 class StatisticsController extends Controller
 {
@@ -17,25 +18,72 @@ class StatisticsController extends Controller
         $chart = new SessionChart();
         //$chartdata = ActiveTime::whereDate('date', '>',Carbon::now()->subDays(3))->groupBy('date')->get();
 
-        $data = collect([]);
+        $logins = collect([]);
+        $time = collect([]);
         $labels = collect([]);
 
         for ($days_backwards = 14; $days_backwards >= 0; $days_backwards--) {
-            // Could also be an array_push if using an array rather than a collection.
-            $data->push(ActiveTime::whereDate('date', today()->subDays($days_backwards))->count());
+            $logins->push(ActiveTime::whereDate('date', today()->subDays($days_backwards))->count());
+            $time->push(ActiveTime::whereDate('date', today()->subDays($days_backwards))->sum('seconds')/60);
             $labels->push(today()->subDays($days_backwards)->toDateString());
         }
+
+        $chart->options([
+            'maintainAspectRatio' => false,
+            'scales'              => [
+                'xAxes' => [],
+                'yAxes' => [ [
+                            'type' => 'linear',
+                            'display' => true,
+                            'position' => 'left',
+                            'id' => 'y-logins'],
+                             [
+                            'type' => 'linear',
+                            'display' => true,
+                            'position' => 'right',
+                             'id' => 'y-time'],
+                            [
+                            'ticks' => [
+                                'beginAtZero' => true,
+                            ],
+                        'position' => 'right'],
+                ],
+            ],
+        ]);
+
         //logger(print_r($data, true));
         $chart->labels($labels);
-        $chart->dataset('Inloggningar per dag', 'line', $data);
+        $chart->dataset('Antal inloggade personer per dag', 'line', $logins)->options([
+            'borderColor' => 'rgba(255, 0, 0, 0.3)',
+            'backgroundColor' => 'rgba(255, 0, 0, 0.1)',
+            'borderWidth' => '3',
+            'yAxisID' => 'y-logins'
+        ]);
+        $chart->dataset('Totalt inloggade minuter per dag', 'line', $time)->options([
+            'borderColor' => 'rgba(0, 255, 0, 0.3)',
+            'backgroundColor' => 'rgba(0, 255, 0, 0.1)',
+            'borderWidth' => '3',
+            'yAxisID' => 'y-time'
+        ]);
         $chart->height(100);
+
+        $projectTime = ProjectTime::all();
+
+        //$attestedLevel1 = TimeAttest::where('level', 1)->sum('hours');
+        //$attestedLevel2 = TimeAttest::where('level', 2)->sum('hours');
+        //$attestedLevel3 = TimeAttest::where('level', 3)->sum('hours');
 
         $data = [
             'sessions' => ActiveTime::whereDate('date', '=', date('Y-m-d'))->count(),
             'users' => User::count(),
             'workplaces' => Workplace::count(),
             'lessons' => Lesson::count(),
+            'totalactivehours' => round(ActiveTime::sum('seconds')/3600, 1),
+            'totalprojecthours' => round($projectTime->sum('minutes')/60, 1),
             'chart' => $chart,
+            'attestedhourslevel1' => TimeAttest::where('attestlevel', 1)->sum('hours'),
+            'attestedhourslevel2' => TimeAttest::where('attestlevel', 2)->sum('hours'),
+            'attestedhourslevel3' => TimeAttest::where('attestlevel', 3)->sum('hours'),
         ];
 
         return view('statistics.index')->with($data);
--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -8,11 +8,16 @@
 <p>@lang('Antal registrerade användare: ') {{$users}}</p>
 <p>@lang('Antal arbetsplatser: ') {{$workplaces}}</p>
 <p>@lang('Antal upplagda lektioner: ') {{$lessons}}</p>
-
+<p>@lang('Antal timmar i plattformen hittills: ') {{$totalactivehours}}</p>
+<p>@lang('Antal timmar manuellt registrerade hittills: ') {{$totalprojecthours}}</p>
+<p>@lang('Antal timmar attesterade av deltagare hittills: ') {{$attestedhourslevel1}}</p>
+<p>@lang('Antal timmar attesterade av arbetsplatskoordinatorer hittills: ') {{$attestedhourslevel2}}</p>
+<p>@lang('Antal timmar attesterade av chefer hittills: ') {{$attestedhourslevel3}}</p>
 
 {!! $chart->container() !!}
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+{{--<script src="/js/highcharts.js"></script>--}}
 {!! $chart->script() !!}
 
 

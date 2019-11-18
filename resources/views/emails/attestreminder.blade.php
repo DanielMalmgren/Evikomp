933308de117ca66b9c5cf37d93d5ade777ad7e37
DanielMalmgren <daniel@kolefors.se>
2019-11-18 06:48:56 +0000

    Changed attest reminder mail text

--- resources/views/emails/attestreminder.blade.php
+++ resources/views/emails/attestreminder.blade.php
@@ -2,11 +2,9 @@
 <head></head>
 <body>
     <p>Hej.</p>
-    <p>Vi vill påminna dig om att du har tid registrerad i projektet Evikomp under {{$month}} som behöver attesteras.</p>
-    <p>Det handlar om {{$hours}} timmar. Det är väldigt viktigt att vi får dessa timmar attesterade för att
-        säkra projektets finansiering.</p>
-    <p><a href="{{env('APP_URL')}}/timeattestlevel1/create">Klicka här och logga in för att komma direkt till attesteringen!</a></p>
-    <a>Med vänlig hälsning</a>
-    <a>Evikomp projektgrupp</a>
+    <p>Vi kan se att du har ägnat {{$hours}} timmar åt projektet Evikomp under {{$month}}. Dessa timmar måste attesteras av dig i projektets lärplattform.</p>
+    <p><a href="{{env('APP_URL')}}/timeattestlevel1/create">För att göra detta, klicka här och logga in så kommer du direkt till attesteringen.</a></p>
+    <p>Med vänlig hälsning, Evikomp projektgrupp</p>
+    <p>OBS! Detta e-postmeddelande är automatgenererat och kan inte besvaras.</p>
 </body>
 </html>

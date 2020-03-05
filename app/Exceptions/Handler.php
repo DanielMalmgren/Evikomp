e5eafd7820baea59172de8fa5d7d68175729b9a4
DanielMalmgren <daniel@kolefors.se>
2020-03-05 08:34:16 +0000

    Upgrade to Laravel 7

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace App\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Auth\AuthenticationException;
 use Aacotroneo\Saml2\Facades\Saml2Auth;
@@ -34,7 +34,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         if (app()->bound('sentry') && $this->shouldReport($exception)){
             app('sentry')->captureException($exception);

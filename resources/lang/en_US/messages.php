e099f44f7ffa1604262baec209c4bad7344dbaf7
DanielMalmgren <daniel@kolefors.se>
2020-03-11 14:31:16 +0000

    Add ESF and SCB to GDPR text

--- resources/lang/en_US/messages.php
+++ resources/lang/en_US/messages.php
@@ -5,7 +5,7 @@ return [
                    <a href="https://www.linkoping.se/utforarwebben/vard-stod-och-omsorg/forskning-och-utveckling/pagaende-projekt/evikomp/">
                    For more information about the project click here.</a><br><br>
                    This web platform is a part of the project. In order to use it your personal data needs to be stored.<br><br>
-                   The only ones able to see your personal data are the system administrators and your boss and workplace administrator.<br>
+                   Your personal data will be available to the system administrators, your boss and workplace administrator and ESF and SCB for follow-up of project financing.<br>
                    Statistical data from the project in anonymized form will be used for research and development.<br><br>
                    By clicking "Continue" below you admit that you have been informed about the abovementioned storage.',
 ];
--- resources/lang/sv_SE/messages.php
+++ resources/lang/sv_SE/messages.php
@@ -5,7 +5,7 @@ return [
                    <a href="https://www.linkoping.se/utforarwebben/vard-stod-och-omsorg/forskning-och-utveckling/pagaende-projekt/evikomp/">
                    För mer information om projektet klicka här.</a><br><br>
                    Denna webplattform är en del av projektet. För att du ska kunna använda den behöver dina personuppgifter lagras.<br><br>
-                   De enda som förutom du själv kan se dina personuppgifter är de systemansvariga samt din chef och arbetsplatskoordinator.<br>
+                   Dina personuppgifter kommer att vara tillgängliga för de systemansvariga, din chef och arbetsplatskoordinator samt personal på Europeiska Socialfonden (ESF) samt Statistiska Centralbyrån (SCB) för uppföljning av projektfinansieringen.<br>
                    Statistiska uppgifter från projektet kommer i anonymiserad form att användas för forskning och utveckling.<br><br>
                    Genom att klicka på "Fortsätt" nedan medger du att du har blivit informerad om ovan beskrivna lagring.',
 ];

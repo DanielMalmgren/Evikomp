ce09f311748128e3f88b3fd8d0eb21acf1ee6dfc
DanielMalmgren <daniel@kolefors.se>
2020-04-09 12:34:27 +0000

    Remove some file transition stuff

--- app/Content.php
+++ app/Content.php
@@ -34,17 +34,6 @@ class Content extends Model
         });
     }
 
-    //Return translated field if exists, else return value directly from contents table
-    //TODO: fix this up once all files has been moved. Only the first line is needed!
-    public function getExistingContentAttribute() {
-        $translation = $this->translateOrDefault(\App::getLocale());
-        if(isset($translation)) {
-            return $translation->text;
-        } else {
-            return $this->content;
-        }
-    }
-
     public function lesson()
     {
         return $this->belongsTo('App\Lesson');
@@ -54,46 +43,31 @@ class Content extends Model
         return Storage::exists("public/files/".$this->id."/".\App::getLocale().'/'.$this->filename());
     }
 
-    public function fallbackLangFileExists() {
-        return Storage::exists("public/files/".$this->id."/".Config::get('app.fallback_locale').'/'.$this->filename());
-    }
-
     public function filename() {
-        return $this->existing_content;
-    }
-
-    //TODO: Remove this once all files has ben transferred to the new naming system!
-    public function filename_oldstyle() {
-        return $this->id.'.'.pathinfo($this->existing_content, PATHINFO_EXTENSION);
+        return $this->translateOrDefault(\App::getLocale())->text;
     }
 
     public function urlpath() {
         if($this->translatedFileExists()) {
             return "/storage/files/".$this->id."/".\App::getLocale().'/';
-        } else if ($this->fallbackLangFileExists()) {
-            return "/storage/files/".$this->id."/".Config::get('app.fallback_locale').'/';
         } else {
-            return "/storage/files/";
+            return "/storage/files/".$this->id."/".Config::get('app.fallback_locale').'/';
         }
     }
 
     public function url() {
         if($this->translatedFileExists()) {
             return "/storage/files/".$this->id."/".\App::getLocale().'/'.$this->filename();
-        } else if ($this->fallbackLangFileExists()) {
-            return "/storage/files/".$this->id."/".Config::get('app.fallback_locale').'/'.$this->filename();
         } else {
-            return "/storage/files/".$this->filename_oldstyle();
+            return "/storage/files/".$this->id."/".Config::get('app.fallback_locale').'/'.$this->filename();
         }
     }
 
     public function filepath($ignoreMissing=false) {
         if($ignoreMissing || $this->translatedFileExists()) {
             return "public/files/".$this->id."/".\App::getLocale().'/';
-        } else if ($this->fallbackLangFileExists()) {
-            return "public/files/".$this->id."/".Config::get('app.fallback_locale').'/';
         } else {
-            return "public/files/";
+            return "public/files/".$this->id."/".Config::get('app.fallback_locale').'/';
         }
     }
 

62b34deb95ec82d40e974f064e33cc9570104b2e
vagrant <vagrant@homestead>
2021-06-07 08:49:02 +0000

    Fix lower case L typo

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\LessonList;
 use App\Track;
-use App\lesson;
+use App\Lesson;
 use App\Workplace;
 
 class ListController extends Controller

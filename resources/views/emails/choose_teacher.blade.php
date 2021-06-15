5ac995ef8bf9e69ecc6736dad32c445c4fe5cd29
DanielMalmgren <daniel@kolefors.se>
2021-06-15 08:29:10 +0000

    Changed wording in mail to learning coordinators

--- resources/views/emails/choose_teacher.blade.php
+++ resources/views/emails/choose_teacher.blade.php
@@ -2,9 +2,9 @@
 <head></head>
 <body>
     <p>Hej.</p>
-    <p>Detta är en notifiering om att en arbetsplats har önskat lärarstöd för ett inplanerat lärtillfälle och att {{$project_time->training_coordinator->name}} har valts som utbildningsanordnare. Ni behöver därför tilldela en lärare till detta lärtillfälle. Gör detta genom att klicka på länken nedan.</p>
+    <p>Detta är en notifiering om att {{$project_time->workplace->name}} har önskat lärarstöd för ett inplanerat lärtillfälle i Evikomp och att {{$project_time->training_coordinator->name}} har valts som utbildningsanordnare. Ni behöver därför tilldela en lärare till detta lärtillfälle. Gör detta genom att klicka på länken nedan.</p>
 
-    <p><a href="{{env('APP_URL')}}/projecttime/{{$project_time->id}}/edit">{{$project_time->workplace->name}}, {{substr($project_time->starttime, 0, 5)}}-{{substr($project_time->endtime, 0, 5)}}</a></p>
+    <p><a href="{{env('APP_URL')}}/projecttime/{{$project_time->id}}/edit">{{$project_time->date}} {{substr($project_time->starttime, 0, 5)}}-{{substr($project_time->endtime, 0, 5)}}</a></p>
 
     <p>Med vänlig hälsning, Evikomp projektgrupp</p>
     <p>Detta e-postmeddelande är automatgenererat.</p>

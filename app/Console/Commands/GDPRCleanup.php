c0c177d2f270be1de73c43a0939a146a3812e7ec
DanielMalmgren <daniel@kolefors.se>
2021-06-21 14:18:01 +0000

    Fix active times dependency problem

--- app/Console/Commands/GDPRCleanup.php
+++ app/Console/Commands/GDPRCleanup.php
@@ -44,6 +44,9 @@ class GDPRCleanup extends Command
         $this->info("Looping through users...");
         $expDate = \Carbon\Carbon::now()->subDays(2);
         foreach(User::where('accepted_gdpr', false)->whereDate('updated_at', '<', $expDate)->get() as $user) {
+            foreach($user->active_times as $active_time) {
+                $active_time->delete();
+            }
             logger("Removing user ".$user->id." who hasn't accepted gdpr");
             $this->info("Deleted ".$user->name);
             $user->delete();

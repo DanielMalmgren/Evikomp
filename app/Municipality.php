c2eab8bef848a6bcce2d517e78bf6e412b8d3cc5
DanielMalmgren <daniel@kolefors.se>
2021-06-24 08:42:26 +0000

    Info per municipality in statistics xls

--- app/Municipality.php
+++ app/Municipality.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Municipality extends Model
 {
@@ -17,5 +18,18 @@ class Municipality extends Model
     public function time_attests()
     {
         return $this->hasManyDeep('App\TimeAttest', ['App\Workplace', 'App\User']);
+    }
+
+    public function users()
+    {
+        return $this->hasManyDeep('App\User', ['App\Workplace']);
+    }
+
+    public function scopeFilter(Builder $query)
+    {
+        return $query->whereHas('workplaces', function($query)
+            {
+                $query->where('includetimeinreports', true);
+            });    
     }
 }

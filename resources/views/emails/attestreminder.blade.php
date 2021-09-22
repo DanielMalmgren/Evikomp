e6ee4c4bc135da42013e22eb0d92e20f8c38c7df
Daniel Malmgren <daniel.malmgren@itsam.se>
2021-09-22 09:15:30 +0000

    Add link to attest info movie in reminder mails

--- resources/views/emails/attestreminder.blade.php
+++ resources/views/emails/attestreminder.blade.php
@@ -4,6 +4,7 @@
     <p>Hej.</p>
     <p>Vi kan se att du har ägnat {{$hours}} timmar åt projektet Evikomp under {{$month}}. Dessa timmar måste attesteras av dig i projektets lärplattform.</p>
     <p><a href="{{env('APP_URL')}}/timeattestlevel1/create">För att göra detta, klicka här och logga in så kommer du direkt till attesteringen.</a></p>
+    <p><a href="https://vimeo.com/588350985/e7ff75797f">Klicka här för att få se en film om attesteringen och varför den är viktig.</a></p>
     <p>Med vänlig hälsning, Evikomp projektgrupp</p>
     <p>Detta e-postmeddelande är automatgenererat.</p>
 </body>
